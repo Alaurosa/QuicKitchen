a3815d9a13b03a2ca1e7bd0c216fdd9eed90298a
Ashley Kim <ashleykim026@gmail.com>
2021-07-21 17:47:48 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:simplymakedemo/home.dart';
 
@@ -29,24 +30,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String displayedText=" ";
 
   void home(){
   Navigator.push(context, MaterialPageRoute(builder: (context) => homePage()));
+  setState(() {
+    displayedText=" ";
+  });
 }
 
   @override
   Widget build(BuildContext context) {
-    var container= Container(
-      padding:EdgeInsets.all(20),
-      width:300,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.blue),
-        borderRadius: BorderRadius.all(
-          Radius.circular(5.0),
-        ),
-      ),
-    );
+      
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("Welcome Back")),
@@ -56,28 +51,57 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              alignment: FractionalOffset(0.2, 0.6),
-              child: Text("Username"),
+              alignment: FractionalOffset(0.1, 0.6),
+              child: Text(
+                "Username",
+                style: TextStyle(fontSize: 18),
+                ),
             ),
             Padding(
-              padding: const EdgeInsets.only(bottom: 15.0),
-              child: container,
-            ),
+                padding: const EdgeInsets.symmetric(horizontal:20.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border:OutlineInputBorder()
+                  ),
+                  style: TextStyle(fontSize: 18.0),
+                  onChanged: (text){
+                    displayedText = text;
+                  },
+                ),
+              ),
             Container(
-              alignment: FractionalOffset(0.2,0.6),
-              child: Text("Password"),
+              alignment: FractionalOffset(0.1,0.6),
+              child: Text(
+                "Password",
+                style: TextStyle(fontSize:18.0),
+              ),
+              padding: const EdgeInsets.only(top:20.0),
             ),
             Padding(
-              padding: const EdgeInsets.only(bottom:8.0),
-              child: container,
-            ),
+              padding: const EdgeInsets.symmetric(horizontal:20.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border:OutlineInputBorder()
+                  ),
+                  style: TextStyle(fontSize: 18.0),
+                  onChanged: (text){
+                    displayedText = text;
+                  },  
+                ),
+              ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text("Forgot your account details?"),
                 Padding(
-                  padding: const EdgeInsets.only(left: 40.0),
-                  child: Text("Sign Up"),
+                  padding: const EdgeInsets.only(left:40.0),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 18),
+                    ),
+                    onPressed: () => {},
+                    child: const Text('Sign Up'),
+                  ),
                 ),
               ],
             ),
